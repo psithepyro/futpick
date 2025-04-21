0088fe1872c5b01d919a38cf0ddbd2539cc11736
psithepyro <psithepyro@gmail.com>
2025-04-20 20:07:47 -0700

    //added a fetch function to get the top assist players and top scorer players // added a new controller for top assist players and top scorer players // added a new route for top assist players and top scorer players but commented it out because it is throwing an error

--- backend/routes/football.js
+++ backend/routes/football.js
@@ -4,10 +4,14 @@ const router = express.Router();
 //import controller functions
 const { getFixtures } = require("../controllers/fixturesController");
 const { getPlayers } = require("../controllers/playersController");
+const { getTopScorers } = require("../controllers/topscorerController");
+const { getTopAssisters } = require("../controllers/topassisterController");
 //import middleware for user authentication
 const authenticateUser = require("../middlewares/jwtMiddleware");
 //defined routes
 router.get("/fixtures", authenticateUser, getFixtures);
 router.get("/players", authenticateUser, getPlayers);
+`router.get("/topscorers", authenticateUser, getTopScorers);
+router.get("/topassisters", authenticateUser, getTopAssisters);`
 
 module.exports = router;
